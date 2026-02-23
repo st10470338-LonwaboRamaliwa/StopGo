ec950d460956a6e4b1aeea92c73a05d4b5683a99
st10470338-LonwaboRamaliwa <ST10470338@myemeris.edu.za>
2026-02-23 14:47:13 +0200

    First Commit

---
+++ src/escapesequenceapp/EscapeSequenceApp.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package escapesequenceapp;
+
+/**
+ *
+ * @author ST10470338
+ */
+public class EscapeSequenceApp {
+
+    public static void main(String[] args) {
+       System.out.println("                 ------------"); //This is the top line before the stop signs
+       System.out.println("   ******** \t\t\t    XXXXXXXX"); // The start of the signs
+       System.out.println(" *          *\t\t\t  X          X");
+       System.out.println("*   S T O P  * \t\t\t X     GO     X"); // words inside the signs
+       System.out.println(" *          * \t\t\t  X          X");
+       System.out.println("   ******** \t\t\t    XXXXXXXX");
+       System.out.println("      **    \t\t\t       XX"); // \t means Tab
+       System.out.println("      **    \t\t\t       XX");
+       System.out.println("      **    \t\t\t       XX");
+       System.out.println("     ****    \t\t\t      XXXX");
+       System.out.println("  **********   \t\t\t   XXXXXXXXXX");
+       System.out.println("|----------------------------------------------|");// out means output
+       System.out.println("|----------------------------------------------|");/*The is the base of the signs*/
+    } /*End of the signs*/
+    
+}
